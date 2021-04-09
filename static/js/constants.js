474797a5155940810492913a2f0e488f9b9b9569
Aarbaz Alam <aarbazlm6@gmail.com>
2021-04-09 04:19:55 +0100

    First Commit

---
+++ static/js/constants.js
@@ -0,0 +1,4 @@
+const action_name = "action_hello_world";
+// const rasa_server_url = "http://localhost:5055/webhook";
+const rasa_server_url = "https://rasacouncildemo.herokuapp.com/webhooks/rest/webhook";
+const sender_id = uuidv4();
